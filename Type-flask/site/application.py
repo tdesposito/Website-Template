208518ea5d924f72d7d3269ea642f9658d27b907
Todd Esposito <todd@toddesposito.com>
2020-08-30 14:51:01 -0500

    fix(Flask): the template app now gives us the "standard" index page.

--- Type-flask/site/application.py
+++ Type-flask/site/application.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template
+)
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    #===== App-level hooks
+    # @app.before_request
+    # def force_https():
+    #     ''' Redirect HTTP to HTTPS in all cases.
+    #
+    #     Note that since we're running on EB, ALL of our requests will come in
+    #     as HTTP (from the load balancer), so we use the X-Forwarded-Proto header
+    #     to determine if the client was using HTTPS or not!
+    #     '''
+    #     if request.headers.get('X-Forwarded-Proto','http').lower() != "https" and app.env != "development":
+    #         return redirect(request.url.replace("http://", "https://", 1), code=301)
+
+
     @app.route('/')
-    def hello():
-        return "Hello World!"
+    def index():
+        return render_template("index.html")
 
     return app
 
